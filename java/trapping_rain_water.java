7b131997461847de4a30efc6770b4d21ef59cc24
Dhedeepya123 <dhedeepya.thakkilapaty@gmail.com>
2026-09-26 23:43:48 +0530

    LeetCode: update java/trapping_rain_water.java

---
+++ java/trapping_rain_water.java
@@ -0,0 +1,31 @@
+// ======================================
+// LeetCode Problem: trapping rain water
+// Language: java
+// Link: https://leetcode.com/problems/trapping-rain-water/
+// Synced by: LinkCode
+// Date: 9/26/2026, 11:43:48 PM
+// ======================================
+
+
+class Solution {
+    public int trap(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int water=0;
+        int left_max=height[left];
+        int right_max=height[right];
+        while(left<right){
+            if(left_max<right_max){
+                left++;
+                left_max=Math.max(left_max,height[left]);
+                water+=left_max-height[left];
+            }
+            else{
+                right--;
+                right_max=Math.max(right_max,height[right]);
+                water+=right_max-height[right];
+            }
+        }
+        return water;
+    }
+}
